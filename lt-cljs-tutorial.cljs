2ddc59a2e2dc952728ef63354e4686dcc78da5ca
David Nolen <david.nolen@gmail.com>
2014-01-15 11:26:33 -0600

    standard lib

--- lt-cljs-tutorial.cljs
+++ lt-cljs-tutorial.cljs
@@ -599,7 +599,35 @@ some-x
 ;; library with the exception of functionality that assumes a multithreaded
 ;; environment, first class namespaces, and Java numerics.
 
-;; Here are some highlights that newcomers to ClojureScript might find useful.
+;; Here are some highlights and patterns that newcomers to ClojureScript might
+;; find useful.
+
+(apply str (interpose ", " ["Bob" "Mary" "George"]))
+
+((juxt :first :last) {:first "Bob" :last "Smith"})
+
+(def people [{:first "John" :last "McCarthy"}
+             {:first "Alan" :last "Kay"}
+             {:first "Joseph" :last "Licklider"}
+             {:first "Robin" :last "Milner"}])
+
+(map :first people)
+
+(take 5 (repeat "red"))
+
+(take 5 (repeat "blue"))
+
+(take 5 (interleave (repeat "red") (repeat "blue")))
+
+(take 10 (cycle ["red" "white" "blue"]))
+
+(partition 2 [:a 1 :b 2 :c 3 :d 4 :e 5])
+
+(partition 2 1 [:a 1 :b 2 :c 3 :d 4 :e 5])
+
+(take-while #(< % 5) (range 10))
+
+(drop-while #(< % 5) (range 10))
 
 
 ;; Protocols

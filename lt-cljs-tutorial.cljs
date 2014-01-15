0a8306d8ae78d7e87ee5e8351e0e8d5ab9c508df
David Nolen <david.nolen@gmail.com>
2014-01-15 07:28:09 -0500

    wip

--- lt-cljs-tutorial.cljs
+++ lt-cljs-tutorial.cljs
@@ -1,8 +1,115 @@
 ;; An Introduction to ClojureScript for Light Table users
-;; ------------------------------------------------------
+;; ============================================================================
 
 ;; To begin open the command pane (Control-SPACE), and Add a Light Table UI
 ;; connection. Once connect you can evaluate all the forms in the file with
 ;; by placing the cursor after the form and typing Command-ENTER.
 
-(ns lt-cljs-tutorial)
+
+;; Declaring a namespaces
+;; ----------------------------------------------------------------------------
+
+;; ClojureScript supports modularity via namespaces. They allow you to group
+;; logical definitions together.
+
+(ns lt-cljs-tutorial
+  (:require [clojure.string :as string]))
+
+;; :require is how you can import functionality from a different namespace into
+;; the current one. Here we are requiring `clojure.string` and giving it an
+;; alias. We could write the following:
+
+(clojure.string/blank? "")
+
+;; But that's really verbose compared to:
+
+(string/blank? "")
+
+
+;; Definitions
+;; ----------------------------------------------------------------------------
+
+;; Once you have a namespace you can start creating top level definitions in
+;; that namespace.
+
+;; You can define a top level with `def`.
+
+(def x 1)
+
+x
+
+;; You can also refer to top level definitions by fully qualifying them.
+
+lt-cljs-tutorial/x
+
+;; This means top levels can never be shadowed by locals and function
+;; parameters.
+
+(let [x 2]
+  lt-cljs-tutorial/x)
+
+;; One way to define a function is like this.
+
+(def y (fn [] 1))
+
+(y)
+
+;; Defining functions in ClojureScript is common enough that `defn` sugar is
+;; provided and idiomatic.
+
+(defn z [] 1)
+
+(z)
+
+
+;; Literal data types
+;; ----------------------------------------------------------------------------
+
+;; ClojureScript comes out of the box with the usual useful data literals.
+
+;; Booleans
+
+(def a-boolean true)
+
+;; Strings
+
+(def a-string "Hello!")
+
+;; Numbers
+
+(def a-number 1)
+
+
+;; JavaScript data type literals
+;; ----------------------------------------------------------------------------
+
+;; You can construct a JavaScript array with the `array` function.
+
+(def an-array (array 1 2 3))
+
+;; But ClojureScript also supports JavaScript data literals via the `#js`
+;; reader literal.
+
+(def another-array #js [1 2 3])
+
+;; Similarly you can create simple JavaScript objects with `js-obj`.
+
+(def an-object (js-obj "foo" "bar"))
+
+;; But again you can save a few characters with `#js`.
+
+(def another-object #js {"foo" "bar"})
+
+;; It's important to note that `#js` is shallow, the contents of `#js` will be
+;; ClojureScript data unless preceded by #js.
+
+;; This is a mutable JavaScript object with an immutable ClojureScript vector
+;; inside.
+
+(def shallow #js {"foo" [1 2 3]})
+
+
+;; Constructing a type
+;; ----------------------------------------------------------------------------
+
+;; Of course there are

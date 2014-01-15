20a2a764983fca64c0c76a29d1972c8ffb967d33
David Nolen <david.nolen@gmail.com>
2014-01-15 07:09:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Light Table ClojureScript Tutorial
+
+An introduction to the ClojureScript programming language aimed at Light Table
+users. You can open `lt-cljs-tutorial.cljs` in Light Table and evaluate it
+interactively by adding a Light Table UI connection.
+
+Pull requests welcome.
---
+++ lt-cljs-tutorial.cljs
@@ -0,0 +1,8 @@
+;; An Introduction to ClojureScript for Light Table users
+;; ------------------------------------------------------
+
+;; To begin open the command pane (Control-SPACE), and Add a Light Table UI
+;; connection. Once connect you can evaluate all the forms in the file with
+;; by placing the cursor after the form and typing Command-ENTER.
+
+(ns lt-cljs-tutorial)

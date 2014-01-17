d068dc5580af175c758716fd636c91bcecd001b1
Josh Ballanco <jballanc@gmail.com>
2014-01-17 23:21:12 +0200

    Add a section explaining comments

--- lt-cljs-tutorial.cljs
+++ lt-cljs-tutorial.cljs
@@ -31,6 +31,41 @@
 (string/blank? "")
 
 
+;; Comments
+;; ----------------------------------------------------------------------------
+
+;; There are three ways to create comments in ClojureScript. The first way is
+;; by preceding a line with a semi-colon, just like the lines you are reading
+;; now.
+
+;; The second way is by proceding a form with `#_`. This causes ClojureScript
+;; to skip the evaluation of only the form immediately following, without
+;; affecting the evaluation of the surrounding forms.
+
+;; Try to reveal the secret message below:
+
+(str "The secret word is " #_(string/reverse "tpircSerujolC"))
+
+;; Finally, you can also create a comment using the `comment` macro. One common
+;; technique is to use the `comment` macro to include code to be evaluated in a
+;; REPL, but which you do not normally want to be included in the compiled
+;; source.
+
+;; For example, try placing your cursor after the last `)` below and type
+;; Command-ENTER:
+
+(comment
+
+  (string/upper-case "This is only a test...")
+
+  )
+
+;; The `comment` macro makes the whole form return `nil`. Now go back and
+;; highlight just the middle line, then type Command-ENTER. In this way
+;; you can include code samples or quick tests in-line with the rest of
+;; your code.
+
+
 ;; Definitions
 ;; ----------------------------------------------------------------------------
 

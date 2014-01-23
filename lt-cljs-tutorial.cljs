48a537943589808f8cf5caa12f3bb5a8f9b32a86
David Nolen <david.nolen@gmail.com>
2014-01-23 08:27:51 -0500

    explain loop/recur

--- lt-cljs-tutorial.cljs
+++ lt-cljs-tutorial.cljs
@@ -443,15 +443,30 @@ a-map
 ;; loop/recur
 ;; ----------------------------------------------------------------------------
 
-;; The most primitive looping construct in ClojureScript is loop/recur. Most
-;; of the iteration constructs are defined in terms of it. Using loop/recur is
-;; usually considered bad style if a reasonable functional solution via
-;; map/filter/reduce or a list comprehension is possible.
+;; The most primitive looping construct in ClojureScript is loop/recur.
+;; Like let you establishing bindings - they will be the initial bindings of
+;; the loop. Like let you may have a sequence of forms for the body. In tail
+;; positions you may write a recur statement that will set the bindings for
+;; the next iteration of the loop. Using loop/recur is usually considered bad
+;; style if a reasonable functional solution via map/filter/reduce or a list
+;; comprehension is possible.
+
+;; While you might write this in JavaScript:
+;;
+;; var ret = [];
+;; for(var i = 0; i < 10; i++) ret.push(i)
+;;
+;; In ClojureScript you would write loop/recur like so:
 
 (loop [i 0 ret []]
   (if (< i 10)
     (recur (inc i) (conj ret i))
     ret))
+
+;; Again avoid loop/recur unless you really need it. The loop above would
+;; be better expressed as the following:
+
+(into [] (range 10))
 
 
 ;; Moar functions
@@ -611,7 +626,7 @@ some-x
                 :or {k 1
                      g 2
                      h 3}}]
-  (hash-map :k k 
+  (hash-map :k k
             :g g
             :h h))
 
@@ -621,7 +636,7 @@ some-x
 (magic :h 1000)
 (magic :k 10 :g 100 :h 1000)
 (magic :h 1000 :k 10 :g 100)
- 
+
 ;; Sequences
 ;; ============================================================================
 
